df20a71a7e9b847fb9f730cab8f13a53b311624d
Aisha Tammy <floss@bsd.ac>
2020-07-24 22:17:15 -0400

    small testing for nt.hh
    Signed-off-by: Aisha Tammy <floss@bsd.ac>

---
+++ number_theory/test_nt.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+#include "nt.hh"
+
+using namespace std;
+using namespace codebook;
+
+int main(){
+  cout << miller_rabin(15) << endl << miller_rabin(21) << endl << miller_rabin(3013) << endl;
+}
