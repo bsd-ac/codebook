5b5e281f5eadd1fd0101e4b75232fbcaa9af4e60
Aisha Tammy <floss@bsd.ac>
2020-07-26 08:50:02 -0400

    fix << operator by doing calculations in MOD
    Signed-off-by: Aisha Tammy <floss@bsd.ac>

--- number_theory/modular.hh
+++ number_theory/modular.hh
@@ -114,7 +114,7 @@ template <int64_t MOD> struct modular {
 
   modular &operator++() { *this += 1; return *this; }
   modular &operator--() { *this -= 1; return *this; }
-  modular &operator<<(const size_t k) { *this *= mpow(2, k); return *this; }
+  modular &operator<<(const size_t k) { *this *= mpow(2, k, MOD); return *this; }
   modular &operator>>(const size_t k) { int64_t p = inverse(2, MOD); *this *= mpow(p, k); return *this; }
 
   operator int64_t() const { return n; }
